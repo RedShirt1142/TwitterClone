f208ccf3c133e9bb7f61503374b031aeaae132ec
RedShirt1142 <red.perc.shirt@icloud.com>
2022-01-13 17:41:50 +0900

    ユーザー画面

---
+++ Views/profile.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../Views/img/logo-twitterblue.svg">
+    <!--Bootstrap CSS -->
+    <!-- getbootstrap.jp(または.com)からjsDelivrの欄からCSSonlyをコピーして、リンクタグのcssの上に貼り付ける -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Views/css/style.css">
+    <!-- JS -->
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous" defer></script>
+    <!-- JavaScript Bundle with Popper -->  <!-- Javaのbootstrapはjqueryに依存してるので、jqueryを先に書く。 -->
+    <!-- 最新版を使いたいときはサイトから最新版のコードを引用すること。code.jquery.com  getbootstrap.jp -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous" defer></script>
+    <!-- いいね用JS -->
+    <script src="../Views/js/likes.js" defer></script>
+
+    <title>プロフィール画面 / Twitterクローン</title>
+    <meta name="description" content="プロフィール画面です">
+</head>
+
+<body class="home profile text-center">
+    <div class="container">
+        <div class="side"> 
+            <div class="side-inner">
+                <ul class="nav flex-column">  
+                    <li class="nav-item"><a href="home.php" class="nav-link"><img src="../Views/img/logo-twitterblue.svg" alt="" class="icon"></a></li>
+                    <li class="nav-item"><a href="home.php" class="nav-link"><img src="../Views/img/icon-home.svg" alt=""></a></li>
+                    <li class="nav-item"><a href="search.php" class="nav-link"><img src="../Views/img/icon-search.svg" alt=""></a></li>
+                    <li class="nav-item"><a href="notification.php" class="nav-link"><img src="../Views/img/icon-notification.svg" alt=""></a></li>
+                    <li class="nav-item"><a href="profile.php" class="nav-link"><img src="../Views/img/icon-profile.svg" alt=""></a></li>
+                    <li class="nav-item"><a href="post.php" class="nav-link"><img src="../Views/img/icon-post-tweet-twitterblue.svg" alt="" class="post-tweet"></a></li>
+                    <li class="nav-item my-icon"><img src="../Views/img_uploaded/user/sample-person.jpg" alt="" class="js-popover"
+                    data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-html="true"
+                    data-bs-content="<a href='profile.php'>プロフィール</a><br><a href='sign-out.php'>ログアウト</a>"
+                    ></li>
+                </ul>
+            </div>
+        </div>
+        <div class="main">
+            <div class="main-header">
+                <h1>太郎</h1>
+            </div>
+            <!-- プロフィールエリア -->
+            <div class="profile-area">
+                <div class="top">
+                    <div class="user"><img src="../Views/img_uploaded/user/sample-person.jpg" alt=""></div>
+
+                    <?php if(isset($_GET['user_id'])) :  ?>
+                        <!-- 相手のページ -->
+                        <?php if(isset($_GET['case'])): ?>
+                            <button class="btn btn-sm btn-reverse">フォローを外す</button>
+                        <?php else: ?>
+                            <button class="btn btn-sm btn-reverse">フォローする</button>
+                        <?php endif; ?>
+                    <?php else: ?>
+                        <!-- 自分のページ -->
+                        <button class="btn btn-reverse btn-sm" data-bs-toggle="modal" data-bs-target="#js-modal">プロフィール編集</button>
+                    <!-- クリックされたときにモーダル機能が実行され、id="js-modal"のモーダルが表示される。 -->
+                    <!-- reverseはどうやらbootstrapらしい。何かを逆にする。smはスモール。小さいボタン。 -->
+                    <?php endif; ?>
+
+                    <div class="modal fade" id="js-modal" tabindex="-1" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <form action="profile.php" method="post" enctype="multipart/form-data">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">プロフィールを編集</h5>
+                                        <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="user">
+                                            <img src="../Views/img_uploaded/user/sample-person.jpg" alt="">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="" class="mb-1">プロフィール写真</label>
+                                            <input type="file" class="form-control form-control-sm" name="image">
+                                        </div>
+
+                                    <input type="text" class="form-control mb-4" name="nickname" value="太郎" placeholder="ニックネーム" maxlength="50" required>
+                                    <input type="text" class="form-control mb-4" name="name" value="taro" placeholder="ユーザー名" maxlength="50" required>
+                                    <input type="email" class="form-control mb-4" name="email" value="taro@techis.jp" placeholder="メールアドレス" maxlength="254" required>
+                                    <input type="password" class="form-control mb-4" name="password" value="" placeholder="パスワードを変更する場合ご入力ください" minlength="4" maxlength="128">
+                                    </div>
+
+                                    <div class="modal-footer">
+                                        <button class="btn btn-reverse" data-bs-dismiss="modal">キャンセル</button>
+                                        <button class="btn" type="submit">保存する</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- modal fadeは、フェードインしてモーダルがひょうじされるクラス tabindex=-1で、タブ操作の対象から外す -->
+                    <!-- エリアって言ったけどaria-hidden=trueで初期状態で表示されないようにする -->
+                </div>
+                <div class="name">太郎</div>
+                <div class="text-muted">@taro</div>
+                <div class="follow-follower">
+                    <div class="follow-count">1</div>
+                    <div class="follow-text">フォロー中</div>
+                    <div class="follow-count">1</div>
+                    <div class="follow-text">フォロワー</div>
+                </div>
+            </div>
+            
+            <!-- 仕切りエリア -->
+            <div class="ditch"></div>
+
+            <!-- TODO : つぶやき一覧エリア -->
+         </div>
+    </div>
+    <!-- HTML内にJavaScriptを書くときは、/bodyタグの前に書くべき -->
+    <script> //第一引数にDOMContentLoadedを書くと、ブラウザがHTMLの解析を完了した時点で第二引数の関数が実行される。
+        document.addEventListener('DOMContentLoaded', function(){
+            $('.js-popover').popover();
+            // popoverは、クリックされて初めて起動する機能。画像とかに重ねたら出るやつじゃない。HAHAHA
+        },false);
+    </script>
+</body>
+</html>
